f8ef1624880591c57d93ef73aaea23e4a4cbe69d
wokular <only1earth4you@gmail.com>
2025-07-25 19:11:30 -0700

    init base map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: { plugins: [tailwindcss()], },
+    vite: {
+        plugins: [tailwindcss()],
+        define: {
+            // Expose the API key to client-side code
+            'import.meta.env.PUBLIC_ARCGIS_API_KEY': JSON.stringify(process.env.apikey)
+        }
+    },
+    integrations: [preact()],
 });
