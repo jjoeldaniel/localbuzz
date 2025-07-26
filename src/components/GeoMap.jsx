76ef742a8106e1a9a6a8c2b6738c544a22146acc
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2025-07-26 13:36:59 -0700

    add basic index map + search bar impl
    invert search bar color

--- src/components/GeoMap.jsx
+++ src/components/GeoMap.jsx
@@ -127,7 +127,7 @@ export default function GeoEnrichedMap() {
     return (
         <div
             ref={viewDiv}
-            style={{ width: '100%', height: '400px', margin: 0, padding: 0 }}
+            style={{ width: '100%', height: '100%', margin: 0, padding: 0 }}
         />
     );
 }
